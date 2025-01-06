83848ca41438b5969daa4c8e6ed9a3c6fa4d8543
David Oche Agbidi <psalmzoe007@gmail.com>
2025-01-06 17:29:45 +0100

    update

--- django-models/relationship_app/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Book
-
-# Function-based view to list all books
-def list_books(request):
-    books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
-    
