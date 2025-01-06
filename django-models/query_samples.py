7096b54bec2ca867d62e07a06978b4393eba8d3f
David Oche Agbidi <psalmzoe007@gmail.com>
2025-01-06 16:20:56 +0100

    new

--- django-models/query_samples.py
+++ django-models/query_samples.py
@@ -19,8 +19,7 @@ def books_in_library(library_name):
 # Retrieve the librarian for a library
 def librarian_of_library(library_name):
     try:
-        library = Library.objects.get(name=library_name)
+        library = Librarian.objects.get(library=library_name)
         return library.librarian
     except (Library.DoesNotExist, Librarian.DoesNotExist):
         return None
-Librarian.objects.get(library=
