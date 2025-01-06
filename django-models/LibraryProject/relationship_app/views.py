8b02c29ed48aa919c548da481a7f43c367ccf082
David Oche Agbidi <psalmzoe007@gmail.com>
2025-01-06 17:20:24 +0100

    new

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -14,3 +14,5 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+    
+from django.views.generic.detail import DetailView
