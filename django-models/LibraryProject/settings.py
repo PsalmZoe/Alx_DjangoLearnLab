7d499fda8a58c38a0f17b17fc559200e3a64fe0b
David Oche Agbidi <psalmzoe007@gmail.com>
2025-01-06 17:31:23 +0100

    new

--- django-models/LibraryProject/settings.py
+++ django-models/LibraryProject/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'LibraryProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],  # Include global template directory
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -68,6 +68,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'LibraryProject.wsgi.application'
 
