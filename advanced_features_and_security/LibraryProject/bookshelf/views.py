f00f7876a7c80aaff1c1034ac178c46c1bb7f37b
David Oche Agbidi <psalmzoe007@gmail.com>
2025-01-07 14:04:47 +0100

    new

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -23,3 +23,27 @@ book.title  # Expected Output: 'Nineteen Eighty-Four'
 book = Book.objects.get(title="Nineteen Eighty-Four")
 book.delete()
 Book.objects.all()  # Expected Output: <QuerySet []>
+
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import permission_required
+from .models import Article
+
+@permission_required('articles.can_view', raise_exception=True)
+def view_article(request, article_id):
+    article = get_object_or_404(Article, id=article_id)
+    return render(request, 'view_article.html', {'article': article})
+
+@permission_required('articles.can_edit', raise_exception=True)
+def edit_article(request, article_id):
+    article = get_object_or_404(Article, id=article_id)
+    if request.method == 'POST':
+        # Handle the form submission and save the article
+        pass
+    return render(request, 'edit_article.html', {'article': article})
+
+@permission_required('articles.can_create', raise_exception=True)
+def create_article(request):
+    if request.method == 'POST':
+        # Handle form submission and create the article
+        pass
+    return render(request, 'create_article.html')
