36f5238fc6474856d806145c044456f5c9b5ec18
David Oche Agbidi <psalmzoe007@gmail.com>
2025-01-07 13:55:15 +0100

    new

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -11,3 +11,27 @@ class BookAdmin(admin.ModelAdmin):
 
     # Search functionality
     search_fields = ('title', 'author')
+
+    from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['email', 'username', 'date_of_birth', 'is_staff']
+    fieldsets = (
+        (None, {'fields': ('email', 'username', 'password')}),
+        ('Personal Info', {'fields': ('date_of_birth', 'profile_photo')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2', 'date_of_birth', 'profile_photo', 'is_staff', 'is_active'),
+        }),
+    )
+    ordering = ['email']
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
