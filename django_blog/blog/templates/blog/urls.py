be610adb00ade5f6fb873788cd9aa2aa3da475a5
David Oche Agbidi <psalmzoe007@gmail.com>
2025-01-08 20:53:45 +0100

    new

--- django_blog/blog/templates/blog/urls.py
+++ django_blog/blog/templates/blog/urls.py
@@ -8,9 +8,6 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
 ]
 
-from django.urls import path
-from . import views
-
 urlpatterns = [
     path('', views.PostListView.as_view(), name='post-list'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
