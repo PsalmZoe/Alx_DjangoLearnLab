38fcff9be54d4a6164affbd43c31896eefde712e
David Oche Agbidi <psalmzoe007@gmail.com>
2025-01-08 18:06:29 +0100

    new

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,11 +1,36 @@
 from django.shortcuts import render
-from django.views.generic import ListView
 from .models import Book
+from django.views.generic import DetailView
+from .models import Library
+from .models import Book, Library
+from django.views.generic.detail import DetailView
+
+# Function-based view to list all books
+def list_books(request):
+    books = Book.objects.all()  # Fetch all books from the database
+    context = {'books': books}
+    return render(request, 'list_books.html', context)
+
+# Class-based view to display library details
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
+
+# Function-based view to list all books
+def list_books(request):
+    books = Book.objects.all()  # Fetch all books from the database
+    context = {'books': books}
+    return render(request, 'relationship_app/list_books.html', context)  # Explicit path
+
+# Class-based view to display library details
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'  # Explicit path
+    context_object_name = 'library'
+
+from django.shortcuts import render
 
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
-class BookListView(ListView):
-    model = Book
-    template_name = 'relationship_app/list_books.html'
-    context_object_name = 'books'
