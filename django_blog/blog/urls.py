47ce73e1fc6bc3dd30bfac43cff9b014c7132593
David Oche Agbidi <psalmzoe007@gmail.com>
2025-01-08 21:15:11 +0100

    new

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('', views.PostListView.as_view(), name='post-list'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
     path('post/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='add-comment'),
-    path('comment/<int:pk>/edit/', views.CommentUpdateView.as_view(), name='edit-comment'),
+    path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='edit-comment'),
     path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='delete-comment'),
 ]
