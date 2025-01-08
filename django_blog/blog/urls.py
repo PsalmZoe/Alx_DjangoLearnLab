48b4b44cdcea925ac0f7fb0cfd111f9e290b625a
David Oche Agbidi <psalmzoe007@gmail.com>
2025-01-08 20:55:54 +0100

    new

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -7,3 +7,11 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
 ]
+
+urlpatterns = [
+    path('', views.PostListView.as_view(), name='post-list'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', views.PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
+]
