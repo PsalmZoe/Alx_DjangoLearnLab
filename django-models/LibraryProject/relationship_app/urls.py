5dae5478d9e8bb041fbe52b382da924b836759f8
David Oche Agbidi <psalmzoe007@gmail.com>
2025-01-08 18:36:07 +0100

    new

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -22,3 +22,12 @@ urlpatterns = [
     path('librarian-view/', views.librarian_view, name='librarian_view'),
     path('member-view/', views.member_view, name='member_view'),
 ]
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('add-book/', views.add_book, name='add_book'),
+    path('edit-book/<int:pk>/', views.edit_book, name='edit_book'),
+    path('delete-book/<int:pk>/', views.delete_book, name='delete_book'),
+]
