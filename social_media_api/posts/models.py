c146597c77eadf8007ab41dbe39e15d20052a39d
David Oche Agbidi <psalmzoe007@gmail.com>
2025-01-09 10:40:19 +0100

    new

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth import get_user_model
+from .models import Post
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -20,3 +22,16 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment by {self.author.username} on {self.post.title}"
+
+User = get_user_model()
+
+class Like(models.Model):
+    user = models.ForeignKey(User, related_name="likes", on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name="likes", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'post')  # Ensure a user can like a post only once
+
+    def __str__(self):
+        return f'{self.user.username} liked {self.post.title}'
